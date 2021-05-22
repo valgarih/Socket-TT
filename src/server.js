683cdbba0be8db6da1f5e7ad24153df1b609b60d
Tu Nombre <you@example.com>
2021-05-21 19:26:57 -0500

    test

---
+++ src/server.js
@@ -0,0 +1,33 @@
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+const cors = require("cors"); 
+
+const corsOptions = { 
+   origin: "*", 
+   credentials: true 
+};
+ 
+app.use(cors(corsOptions));
+
+io.on('connection', (socket) => {
+ 
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.username, event: 'left'});   
+  });
+ 
+  socket.on('set-name', (name) => {
+    socket.username = name;
+    io.emit('users-changed', {user: name, event: 'joined'});    
+  });
+  
+  socket.on('send-message', (message) => {
+    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});    
+  });
+});
+ 
+var port = process.env.PORT || 3001;
+ 
+server.listen(port, function(){
+   console.log('listening in http://localhost:' + port);
+});
